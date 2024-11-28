639208b5e2d7cb3efe510c05c81df795d240eaa7
Pierre HELLICH <pierre.hellich@elca.ch>
2024-11-28 17:57:59 +0100

    dockercompose qui marche, paas encore colcon build pour creer le package ros

--- docker_humble_desktop/build.sh
+++ docker_humble_desktop/build.sh
@@ -1,1 +1,5 @@
+##############
+# DEPRECATED #
+##############
+
 docker build --pull --no-cache --progress=plain -t ghcr.io/epflxplore/base:humble-desktop -f Dockerfile ..
--- docker_humble_desktop/run.sh
+++ docker_humble_desktop/run.sh
@@ -15,44 +15,52 @@ if [ ! -f $XAUTH ]; then
 fi
 
 echo "Done."
-echo ""
-echo "Verifying file contents:"
-file $XAUTH
-echo "--> It should say \"X11 Xauthority data\"."
-echo ""
-echo "Permissions:"
-ls -FAlh $XAUTH
-echo ""
+# echo ""
+# echo "Verifying file contents:"
+# file $XAUTH
+# echo "--> It should say \"X11 Xauthority data\"."
+# echo ""
+# echo "Permissions:"
+# ls -FAlh $XAUTH
+# echo ""
 echo "Running docker..."
 
-# Get the current working directory
-current_dir=$(pwd)
+# Build the container only if the image needs to be built
+echo "Building the container with Docker Compose..."
+docker-compose build
 
-# Use dirname to get the parent directory
-parent_dir=$(dirname "$current_dir")
+# Run the container
+echo "Running the container with Docker Compose..."
+docker-compose run --service-ports vocal_command /bin/bash
+# démarre container pour service vocal_command + ouvre un terminal interactif avec /bin/bash
 
-# Add this line if you want to use sound: --device /dev/snd your_image_name \ 
+# sudo apt-get install -y docker-compose
 
-# docker build  -f Dockerfile  . -t
-docker build -f Dockerfile . -t epflxplore/vocal_command:latest
+# # Get the current working directory
+# current_dir=$(pwd)
 
+# # Use dirname to get the parent directory
+# parent_dir=$(dirname "$current_dir")
 
-docker run -it \
-    --name base_humble_desktop \
-    --rm \
-    --privileged \
-    --net=host \
-    -e DISPLAY=unix$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -e XAUTHORITY=$XAUTH \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v $XAUTH:$XAUTH \
-    -v /run/user/1000/at-spi:/run/user/1000/at-spi \
-    -v /dev:/dev \
-    -v $parent_dir:/home/xplore/dev_ws/src \
-    -v base_humble_desktop_home_volume:/home/xplore \
-    --device /dev/snd \
-    ghcr.io/epflxplore/base:humble-desktop
+# docker build -f Dockerfile . -t ghcr.io/epflxplore/vocal_command:latest
 
-# pour run docker compose:
-# docker-compose up
+# # Add this line if you want to use sound: --device /dev/snd your_image_name \ 
+# docker run -it \
+#     --name base_humble_desktop \
+#     --rm \
+#     --privileged \
+#     --net=host \
+#     -e DISPLAY=unix$DISPLAY \
+#     -e QT_X11_NO_MITSHM=1 \
+#     -e XAUTHORITY=$XAUTH \
+#     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+#     -v $XAUTH:$XAUTH \
+#     -v /run/user/1000/at-spi:/run/user/1000/at-spi \
+#     -v /dev:/dev \
+#     -v $parent_dir:/home/xplore/dev_ws/src \
+#     -v base_humble_desktop_home_volume:/home/xplore \
+#     --device /dev/snd \
+#     ghcr.io/epflxplore/vocal_command:latest
+
+# # pour run docker compose:
+# # docker-compose up --build
